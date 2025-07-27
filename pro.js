06d26dff1171b4ea31f12e4e7ee49d282d7181e3
sugat jmgade <jamgdesugat42@gmail.com>
2025-07-28 02:28:09 +0530

    this final code

--- pro.js
+++ pro.js
@@ -2,10 +2,10 @@
 
 function add(){
 
-const name = document.getElementById(name);
-const it =document.getElementById(it);
+const name = document.getElementById("name");
+const it =document.getElementById("it");
 
-const button =document.getElementById(button);
+const button =document.getElementById("button");
 
 name.style.backgroundColor = "yellow";
 name.style.Color = "black";
@@ -14,7 +14,7 @@ name.style.Color = "black";
 it.style.border = "2px solid green";
 
 it.style.height = "56px";
-it.style.width= "28px";
+it.style.width= "128px";
 
 
 
